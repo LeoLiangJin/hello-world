e69c093b6bb6fba6f8acb107d70036c02c2b3476
Leo <linzi1158501792@gmail.com>
2017-12-08 17:47:56 +1300

    test java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,20 @@
-public class HelloWorld {
-
+public class HelloWorld extends Father {
+		
+	protected int a = 1323;
+	
 	public static void main(String[] args) {
 		System.out.println("HelloWorld");	
-			
+		Father t = new HelloWorld();
+		System.out.println(((HelloWorld)t).a);	
 	}
+
+	
+
+}
+
+
+class Father {
+
+	protected int a = 124444;
 
 }
